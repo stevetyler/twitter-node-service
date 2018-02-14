063db24e874bfeadca7274fdd4b650eb8fb52f81
Steve Tyler <stevetyler.uk@gmail.com>
2018-02-14 17:58:45 +0000

    added formatTweets function to service

--- services/twitter.js
+++ services/twitter.js
@@ -3,7 +3,6 @@ const Twit = require('twit');
 const client = new Twit({
   consumer_key: twitterAuth.consumer_key,
   consumer_secret: twitterAuth.consumer_secret,
-  //app_only_auth: true,
   access_token: twitterAuth.access_token,
   access_token_secret: twitterAuth.access_token_secret,
   timeout_ms: 60*1000  // optional HTTP request timeout to apply to all requests.
@@ -14,9 +13,19 @@ exports.fetchUserTweets = async (user_id) => {
   const params = {
     user_id: user_id,
   };
-
   let tweets = await client.get('statuses/user_timeline', params);
+  //console.log(tweets.data);
+  return tweets.data;
+}
 
-  console.log(tweets.data);
+exports.formatTweets = (data) => {
+  return data.map(tweet => formatTweet(tweet));
+}
 
+function formatTweet(tweet) {
+  return {
+    body: tweet.text,
+    createdDate: tweet.created_at,
+    author: tweet.user.screen_name
+  }
 }
