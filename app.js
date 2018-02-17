14860ff44458d97cbb4fbcbb5b48590c6efbbb39
Steve Tyler <mail@steve-tyler.co.uk>
2018-02-17 17:58:36 +0000

    updated app for app-only auth

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const https = require('https');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
-var index = require('./routes/index');
+const index = require('./routes/index');
+const options = {
+    key: fs.readFileSync( './localhost.key' ),
+    cert: fs.readFileSync( './localhost.cert' ),
+    requestCert: false,
+    rejectUnauthorized: false
+};
 
-var app = express();
+const app = express();
+const port = process.env.PORT || 443;
+const server = https.createServer( options, app );
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -25,7 +36,7 @@ app.use('/', index);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
--- services/twitter.js
+++ services/twitter.js
@@ -3,11 +3,9 @@ const Twit = require('twit');
 const client = new Twit({
   consumer_key: twitterAuth.consumer_key,
   consumer_secret: twitterAuth.consumer_secret,
-  access_token: twitterAuth.access_token,
-  access_token_secret: twitterAuth.access_token_secret,
+  app_only_auth: true,
   timeout_ms: 60*1000  // optional HTTP request timeout to apply to all requests.
 });
-
 
 exports.fetchUserTweets = async (user_id) => {
   const params = {
